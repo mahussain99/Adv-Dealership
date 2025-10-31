cc0d63ce19010c249ef434cb4619a42bbc0fdf43
Family <mh5212357@gmail.com>
2025-10-31 16:54:44 -0400

    Still i am working on it

---
+++ src/main/java/com/pluralsight/ContractDataManager.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.io.FileWriter;
+
+import java.io.*;
+
+public class ContractDataManager {
+    private static final String FILE_PATH_NAME = "contracts.csv";
+
+    public void saveContract(Contract contract) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH_NAME, true))) {
+
+
+            if (contract instanceof SalesContract) {
+                writer.write("SALE|" + contract.getContractDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" + contract.getvehicleSold().getVin() + "|" + contract.getvehicleSold().getYear() + "|" + contract.getvehicleSold().getMake()
+                        + "|" + contract.getvehicleSold().getModel() + "|" + contract.getvehicleSold().getVehicleType() + "|" + contract.getvehicleSold().getColor() + "|" + contract.getvehicleSold().getOdometer() + "|" + contract.getvehicleSold().getPrice() + "|"
+                        + ((SalesContract) contract).getSalesTaxAmount() + "|" + ((SalesContract) contract).getvehicleSold() + "|" + ((SalesContract) contract).getProcessingFee() + "|" + contract.getTotalPrice() + "|" + ((SalesContract) contract).isFinanceOption() + contract.getMonthlyPayment());
+            } else if (contract instanceof LeaseContract) {
+                writer.write("LEASE|" + contract.getContractDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" + contract.getvehicleSold().getVin() + "|" + contract.getvehicleSold().getYear() + "|" + contract.getvehicleSold().getMake()
+                        + "|" + contract.getvehicleSold().getModel() + "|" + contract.getvehicleSold().getVehicleType() + "|" + contract.getvehicleSold().getColor() + "|" + contract.getvehicleSold().getOdometer() + "|" + contract.getvehicleSold().getPrice() + "|"
+                        + ((LeaseContract) contract).getExpectedEndingValue() + "|" + ((LeaseContract) contract).getLeaseFee() + "|" + contract.getTotalPrice() + "|" + contract.getMonthlyPayment());
+            }
+        } catch (Exception e) {
+            System.err.println("Contract file is not save.");
+        }
+    }
+}
+
---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,65 @@
+package com.pluralsight;
+
+public class SalesContract extends Contract {
+
+    private boolean financeOption;
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+
+    public SalesContract(String contractDate, String customerName, String customerEmail, Vehicle vehicleSold ,
+                         boolean financeOption, double salesTaxAmount, double recordingFee, double processingFee) {
+        super(contractDate, customerName, customerEmail, vehicleSold);
+        this.financeOption = financeOption;
+        this.salesTaxAmount = vehicleSold.getPrice() * 0.05;
+        this.recordingFee = 100.00;
+        if (getVehicleSold().getPrice() > 1000) {
+        this.processingFee = 495;
+    } else{
+        this.processingFee = 295;
+        this.financeOption = financeOption;
+}
+
+    }
+
+    public boolean isFinanceOption() {
+        return financeOption;
+    }
+
+    public double getSalesTaxAmount() {
+        return salesTaxAmount;
+    }
+
+    public double getProcessingFee() {
+        return processingFee;
+    }
+
+
+    @Override
+    public double getTotalPrice() {
+        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
+    }
+
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 0;
+        double interestRate = 0;
+        if (financeOption) {
+            if (getVehicleSold().getPrice() >= 10000) {
+                numberOfPayments = 48;
+                interestRate = 4.25 / 1200;
+            } else {
+                numberOfPayments = 24;
+                interestRate = 5.25 / 1200;
+            }
+
+            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+            monthlyPayment = Math.round(monthlyPayment * 100);
+            monthlyPayment /= 100;
+            return monthlyPayment;
+        } else {
+            return 0.0;
+        }
+    }
+}
